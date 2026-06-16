ad79b67f1ca6ad6667f28ddc6d92b97f54d04d8a
hex0x0 <sqh1107@gmail.com>
2026-06-16 15:10:18 +0800

    Upgrade scripts

--- demo/start-my-app-gateway-for-be.sh
+++ demo/start-my-app-gateway-for-be.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export http_proxy=http://127.0.0.1:8889
-
 container_name=my-app-gateway
 
 docker pull hex0x0/my-app-gateway:latest
@@ -13,3 +11,4 @@ docker rm ${container_name}
 docker rmi $(docker images -f "dangling=true" -q)
 
 docker run --name ${container_name} -d -p 10443:10443 hex0x0/my-app-gateway
+
--- demo/start-my-app-gateway-for-fe.sh
+++ demo/start-my-app-gateway-for-fe.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export https_proxy=http://127.0.0.1:8889
-
 container_name=gateway-my-app-fe
 
 docker pull hex0x0/my-app-gateway:latest
@@ -14,3 +12,4 @@ docker rmi $(docker images -f "dangling=true" -q)
 
 # docker network create --driver=bridge --subnet=192.168.100.0/24 nginx
 docker run --name ${container_name} -d --network=nginx -p 80:80 hex0x0/my-app-gateway
+
