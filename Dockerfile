aa9c107831597e82cddb179636973124ba984b58
hex0x0 <sqh1107@gmail.com>
2026-06-16 15:09:20 +0800

    Upgrade docker image dependency

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM nginx:1.31.0
+FROM nginx:1.31.1
 COPY reverse_proxy.conf /etc/nginx/conf.d/reverse_proxy.conf
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY ssl /etc/nginx/ssl
 RUN unlink /var/log/nginx/access.log
 RUN unlink /var/log/nginx/error.log
+
--- build-and-push-image.sh
+++ build-and-push-image.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export https_proxy=http://127.0.0.1:8889
-
 docker stop my-app-gateway
 
 docker rm my-app-gateway
@@ -11,3 +9,4 @@ docker build --network=host -t hex0x0/my-app-gateway .
 docker rmi $(docker images -qa -f 'dangling=true')
 
 docker push hex0x0/my-app-gateway
+
